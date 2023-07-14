4db2a67abc8dd975db82788b2056669f8a2d2c54
Andrew Spies <andrew.spies@xneelo.com>
2023-07-14 16:40:04 +0200

    add comments for testing docs

--- client_app/src/services/user.ts
+++ client_app/src/services/user.ts
@@ -4,6 +4,7 @@ import APIService from "./api";
 
 export interface User {
   id: number;
+  name?: string;
   email?: string;
 }
 
@@ -12,26 +13,66 @@ export default class UserService extends APIService {
     super();
   }
 
-  public async getUsers(): Promise<any> {
+  /**
+   * getUsers()
+   * 
+   * Get all users
+   * 
+   * @returns {Promise<any>} Promise object represents users
+   * @returns {Promise<User[]>} Promise object represents users
+   */
+  public async getUsers(): Promise<User[]> {
     return await this.request('GET', 'users');
   }
 
-  public async getUser(id: number): Promise<any> {
+  /** 
+   * getUser(id: number)
+   * 
+   * Get user by id
+   * 
+   * @param {number} id User id
+   * @returns {Promise<User>} Promise object represents user
+   */
+  public async getUser(id: number): Promise<User> {
     const user: User = {
-      id
+      id,
     };
     return await this.request('POST', 'users', user);
   }
 
-  public async createUser(user: User): Promise<any> {
+  /**
+   * createUser(user: User)
+   * 
+   * Create new user
+   * 
+   * @param {User} user User object
+   * @returns {Promise<any>} Promise object represents user
+   */
+  public async createUser(user: User): Promise<User> {
     return await this.request('POST', 'users', user);
   }
 
-  public async updateUser(user: User): Promise<any> {
+  /**
+   * updateUser(user: User)
+   * 
+   * Update user
+   * 
+   * @param {User} user User object
+   * @returns {Promise<any>} Promise object represents user
+   */
+  public async updateUser(user: User): Promise<User> {
     return await this.request('POST', 'users', user);
   }
 
-  public async deleteUser(id: number): Promise<any> {
+  /**
+   * deleteUser(id: number)
+   * 
+   * Delete user
+   * 
+   * @param {number} id User id
+   * @returns {Promise<any>} Promise object represents user
+   */
+  public async deleteUser(id: number): Promise<User> {
     const user: User = {
       id
     };
